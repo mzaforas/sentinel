a9b6ec6f74b804304077eacc517031281bd45332
mzaforas <mzaforas@gmail.com>
2014-02-05 23:08:22 +0100

    glob a True en fabric put

--- scripts/transmission_sentinel.py
+++ scripts/transmission_sentinel.py
@@ -7,6 +7,7 @@ import os.path
 import sys
 from fabric.api import env
 from fabric.operations import run, put
+import traceback
 
 from credentials import *
 
@@ -17,23 +18,23 @@ server.login(mail_username, mail_password)
 client = transmissionrpc.Client(address=transmission_host, user=transmission_username, password=transmission_password)
 
 def scp_torrent(torrent):
-    src_path = os.path.normpath(unicode(torrent.downloadDir) + u'/' + unicode(torrent.name))
+    src_path = os.path.normpath(torrent.downloadDir + '/' + torrent.name)
     dst_path = os.path.normpath(u"/media/STOREX/Descargas/")
 
     env.host_string = 'xbmc'
     try:
-        res = put(src_path, dst_path)
+        res = put(src_path, dst_path, use_glob=False)
         if res.failed:
             raise Exception
     except:
-        msg = 'Subject: Transmission sentinel error\n\nError when trying move torrent finished: {name}\n\n{exception}'.format(name=torrent.name, exception=sys.exc_info()[0])
+        msg = 'Subject: Transmission sentinel error\n\nError when trying move torrent finished: {name}\n\n{exception}\n\n{traceback}'.format(name=torrent.name, exception=sys.exc_info(), traceback=traceback.format_exc())
         server.sendmail(fromaddr, toaddrs, msg)
         return False
 
     return True
 
 for torrent in client.get_torrents():
-    if torrent.progress == 100.0:
+    if torrent.progress == 100.0 and torrent.status == 'seeding':
 
         torrent.stop()
 
