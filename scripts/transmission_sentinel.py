074d86a8e95b6812b2f1c7d924b1571d1bee21bc
mzaforas <mzaforas@gmail.com>
2014-02-06 19:21:38 +0100

    Better email connection management. Avoid session expired

--- scripts/transmission_sentinel.py
+++ scripts/transmission_sentinel.py
@@ -11,11 +11,14 @@ import traceback
 
 from credentials import *
 
-server = smtplib.SMTP('smtp.gmail.com:587')  
-server.starttls()  
-server.login(mail_username, mail_password)  
 
-client = transmissionrpc.Client(address=transmission_host, user=transmission_username, password=transmission_password)
+def notify(msg):
+    server = smtplib.SMTP('smtp.gmail.com:587')  
+    server.starttls()  
+    server.login(mail_username, mail_password)  
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
 
 def scp_torrent(torrent):
     src_path = os.path.normpath(torrent.downloadDir + '/' + torrent.name)
@@ -28,23 +31,21 @@ def scp_torrent(torrent):
             raise Exception
     except:
         msg = 'Subject: Transmission sentinel error\n\nError when trying move torrent finished: {name}\n\n{exception}\n\n{traceback}'.format(name=torrent.name, exception=sys.exc_info(), traceback=traceback.format_exc())
-        server.sendmail(fromaddr, toaddrs, msg)
+        notify(msg)
         return False
 
     return True
 
+client = transmissionrpc.Client(address=transmission_host, user=transmission_username, password=transmission_password)
+
 for torrent in client.get_torrents():
     if torrent.progress == 100.0 and torrent.status == 'seeding':
-
         torrent.stop()
-
         copied = scp_torrent(torrent)
 
         if copied:
             client.remove_torrent(torrent.id, delete_data=True)
-
-            msg = u'Subject: Transmission sentinel notification\n\nTorrent finished: %s' % torrent.name
-            server.sendmail(fromaddr, toaddrs, msg)
+            notify(u'Subject: Transmission sentinel notification\n\nTorrent finished: %s' % torrent.name)
             
 
-server.quit()  
+
